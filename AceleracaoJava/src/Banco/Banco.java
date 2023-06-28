1b940d1f7a2e3510b8c0baf50b65fbd4a831f412
GustavoSFer <gustavo.sfernander@gmail.com>
2023-06-27 21:23:10 -0300

    Fazendo a conversão de string para int

--- AceleracaoJava/src/Banco/Banco.java
+++ AceleracaoJava/src/Banco/Banco.java
@@ -13,6 +13,21 @@ public class Banco {
     String nome = teclado.next();
     System.out.println("Bem vindo " + nome);
 
+    System.out.println("================");
+
+    System.out.print("Informe uma numero: ");
+    String numero1 = teclado.next();
+
+    System.out.print("Informe outro numero: ");
+    String numero2 = teclado.next();
+
+    int a = Integer.parseInt(numero1);
+    int b = Integer.parseInt(numero2);
+
+    int result = a + b;
+    System.out.println("A soma é: " + result);
+
+
     teclado.close();
 
   }
