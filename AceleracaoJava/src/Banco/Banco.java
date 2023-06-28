4761d2e40cfd05766a08e2c163096e988f52120a
GustavoSFer <gustavo.sfernander@gmail.com>
2023-06-27 21:17:00 -0300

    usando o scanner para receber informações

--- AceleracaoJava/src/Banco/Banco.java
+++ AceleracaoJava/src/Banco/Banco.java
@@ -1,10 +1,19 @@
 package Banco;
 
+import java.util.Scanner;
+
 public class Banco {
 
   public static void main(String[] args) {
     ContaBancaria obj = new ContaBancaria();
     obj.depositar(25);
+
+    Scanner teclado = new Scanner(System.in);
+    System.out.print("Informe seu nome: ");
+    String nome = teclado.next();
+    System.out.println("Bem vindo " + nome);
+
+    teclado.close();
 
   }
 
