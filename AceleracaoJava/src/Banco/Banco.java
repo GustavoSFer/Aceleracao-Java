65e442561c1f7c708138b018b72367ba2894f6c3
GustavoSFer <gustavo.sfernander@gmail.com>
2023-06-27 21:11:13 -0300

    Criando uma programa simples de banco, que tem o método de deposito.

---
+++ AceleracaoJava/src/Banco/Banco.java
@@ -0,0 +1,11 @@
+package Banco;
+
+public class Banco {
+
+  public static void main(String[] args) {
+    ContaBancaria obj = new ContaBancaria();
+    obj.depositar(25);
+
+  }
+
+}
---
+++ AceleracaoJava/src/Banco/ContaBancaria.java
@@ -0,0 +1,8 @@
+package Banco;
+
+public class ContaBancaria {
+
+  public void depositar(int valor) {
+    System.out.println("Parabens, você depositou: " + valor);
+  }
+}
