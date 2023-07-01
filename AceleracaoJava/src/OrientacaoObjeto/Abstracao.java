9a2de86e1454ace6d43bf30b1dfcdcc6eb3d76de
GustavoSFer <gustavo.sfernander@gmail.com>
2023-07-01 19:53:53 -0300

    colocando um metodo e atributo de paginas

--- AceleracaoJava/src/OrientacaoObjeto/Abstracao.java
+++ AceleracaoJava/src/OrientacaoObjeto/Abstracao.java
@@ -3,8 +3,9 @@ package OrientacaoObjeto;
 public class Abstracao {
 
   public static void main(String[] args) {
-    AbstracaoLivro livro1 = new AbstracaoLivro("Código limpo");
+    AbstracaoLivro livro1 = new AbstracaoLivro("Código limpo", 358);
     System.out.println("O titulo do livro1 é: " + livro1.getNome());
+    System.out.println("O livro tem " + livro1.getNumeroDePaginas() + " paginas!");
 
   }
 
--- AceleracaoJava/src/OrientacaoObjeto/AbstracaoLivro.java
+++ AceleracaoJava/src/OrientacaoObjeto/AbstracaoLivro.java
@@ -2,12 +2,18 @@ package OrientacaoObjeto;
 
 public class AbstracaoLivro {
   private String nome;
+  private int numeroPagina;
 
-  AbstracaoLivro(String nome) {
+  AbstracaoLivro(String nome, int pagina) {
     this.nome = nome;
+    this.numeroPagina = pagina;
   }
 
   public String getNome() {
     return this.nome;
+  }
+
+  public int getNumeroDePaginas() {
+    return this.numeroPagina;
   }
 }
