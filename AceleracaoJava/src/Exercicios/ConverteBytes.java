2d4b7c9bb4fa295e4f417c3c7e1f7dd03d0e1cd1
GustavoSFer <gustavo.sfernander@gmail.com>
2023-07-02 20:43:55 -0300

    Convertendo de bytes para bits

---
+++ AceleracaoJava/src/Exercicios/ConverteBytes.java
@@ -0,0 +1,24 @@
+package Exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ConverteBytes {
+  /**
+   * Mostrando a quantidade de bits.
+   */
+  public static void main(String[] args) {
+    // Ler a entrada de um número pelo console
+    Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+
+    System.out.println("Digite um número:");
+    byte valor = scan.nextByte();
+
+    int qtdBits = valor * 8;
+
+
+    // Imprime a saída no console. OBS: descomente a próxima linha
+    System.out.println(valor + " bytes correspondem a " + qtdBits + " bits");
+
+  }
+}
