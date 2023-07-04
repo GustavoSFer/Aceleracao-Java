dafe7fcf2baffc4440e30ee6da6bf25a06d84639
GustavoSFer <gustavo.sfernander@gmail.com>
2023-07-03 21:13:04 -0300

    Lampada

---
+++ AceleracaoJava/src/Bloco1/Lampada.java
@@ -0,0 +1,14 @@
+package Bloco1;
+
+public class Lampada {
+  /**
+   * Lampada ligada ou desligada.
+   */
+  public static void main(String[] args) {
+    boolean isLigada = true;
+
+    // Imprime a saída no console. OBS: descomente a próxima linha
+    System.out.println("A lampada ligada: " + isLigada + "!");
+
+  }
+}
