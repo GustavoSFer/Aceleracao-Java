6a66454ba8b6254275b24517d82b15a01aa1ba75
GustavoSFer <gustavo.sfernander@gmail.com>
2023-07-09 13:42:33 -0300

    Iniciando um array

---
+++ AceleracaoJava/src/Exercicios/arraySoma.java
@@ -0,0 +1,12 @@
+package Exercicios;
+
+public class arraySoma {
+
+  public static void main(String[] args) {
+    int[] a = {1, 0, 5, -2, -5, 7};
+    // int[] b = new int[6]; outra forma de iniciar um array, mas sem os numeros.
+
+
+  }
+
+}
