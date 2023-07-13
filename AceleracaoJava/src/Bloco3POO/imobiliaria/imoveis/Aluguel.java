679c6fac2a1163033e3970d0cc0f11fca6bb5aaa
GustavoSFer <gustavo.sfernander@gmail.com>
2023-07-13 20:57:31 -0300

    Criando a classe aluguel

--- AceleracaoJava/src/Bloco3POO/imobiliaria/imoveis/Aluguel.java
+++ AceleracaoJava/src/Bloco3POO/imobiliaria/imoveis/Aluguel.java
@@ -1,5 +1,16 @@
 package Bloco3POO.imobiliaria.imoveis;
 
-public class Aluguel {
+import java.time.LocalDate;
+
+public class Aluguel extends Imovel {
+
+  private double valorAluguel;
+  private boolean alugado;
+
+  public Aluguel(String endereco, LocalDate date, double valorAluguel) {
+    super(endereco, date);
+    this.valorAluguel = valorAluguel;
+    this.alugado = false;
+  }
 
 }
