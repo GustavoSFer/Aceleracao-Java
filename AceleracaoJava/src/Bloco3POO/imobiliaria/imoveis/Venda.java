be930b84b6bda764bc431d445958548f13f01b0f
GustavoSFer <gustavo.sfernander@gmail.com>
2023-07-13 20:52:50 -0300

    Criando a classe venda

--- AceleracaoJava/src/Bloco3POO/imobiliaria/imoveis/Venda.java
+++ AceleracaoJava/src/Bloco3POO/imobiliaria/imoveis/Venda.java
@@ -1,5 +1,16 @@
 package Bloco3POO.imobiliaria.imoveis;
 
-public class Venda {
+import java.time.LocalDate;
+
+public class Venda extends Imovel {
+  private double valorVenda;
+  private boolean alugado;
+
+  public Venda(String endereco, LocalDate date, double valorVenda) {
+    super(endereco, date);
+    this.valorVenda = valorVenda;
+    this.alugado = false;
+  }
+
 
 }
