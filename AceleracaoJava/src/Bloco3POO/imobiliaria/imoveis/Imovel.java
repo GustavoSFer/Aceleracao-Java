fbe76d42843f8e302cee1879bef03cbb23e8a479
GustavoSFer <gustavo.sfernander@gmail.com>
2023-07-13 20:50:24 -0300

    Criando os métodos getters and setters.

--- AceleracaoJava/src/Bloco3POO/imobiliaria/imoveis/Imovel.java
+++ AceleracaoJava/src/Bloco3POO/imobiliaria/imoveis/Imovel.java
@@ -14,6 +14,30 @@ public class Imovel {
     this.ativo = false;
   }
 
+  public String getEndereco() {
+    return endereco;
+  }
+
+  public void setEndereco(String endereco) {
+    this.endereco = endereco;
+  }
+
+  public LocalDate getDate() {
+    return date;
+  }
+
+  public void setDate(LocalDate date) {
+    this.date = date;
+  }
+
+  public boolean isAtivo() {
+    return ativo;
+  }
+
+  public void ativarImovel() {
+    this.ativo = true;
+  }
+
 
 
 }
