5650293c9e265d125e0dba48cb3988b169eb2fd1
GustavoSFer <gustavo.sfernander@gmail.com>
2023-07-13 21:07:19 -0300

    Criando um objeto de aluguel.

--- AceleracaoJava/src/Bloco3POO/imobiliaria/main/MainImovel.java
+++ AceleracaoJava/src/Bloco3POO/imobiliaria/main/MainImovel.java
@@ -1,6 +1,7 @@
 package Bloco3POO.imobiliaria.main;
 
 import java.time.LocalDate;
+import Bloco3POO.imobiliaria.imoveis.Aluguel;
 import Bloco3POO.imobiliaria.imoveis.Venda;
 
 public class MainImovel {
@@ -9,6 +10,12 @@ public class MainImovel {
     Venda apto1 = new Venda("Rua Consolação, 255", LocalDate.now(), 600000);
     System.out.println("Endereço do apartamento: " + apto1.getEndereco());
     System.out.println("O valor do venda é: R$ " + apto1.getValorVenda());
+
+    Aluguel apto2 = new Aluguel("Rua algusta, 685", LocalDate.now(), 2300);
+    System.out.println("Endereço do apartamento: " + apto2.getEndereco());
+    System.out.println("O valor do venda é: R$ " + apto2.getValorAluguel());
+
+
 
   }
 
