19859a2344f2c23b5bb0878652b587ac9919478c
GustavoSFer <gustavo.sfernander@gmail.com>
2023-07-13 21:05:11 -0300

    Criando um objeto de venda

--- AceleracaoJava/src/Bloco3POO/imobiliaria/main/MainImovel.java
+++ AceleracaoJava/src/Bloco3POO/imobiliaria/main/MainImovel.java
@@ -1,9 +1,14 @@
 package Bloco3POO.imobiliaria.main;
 
+import java.time.LocalDate;
+import Bloco3POO.imobiliaria.imoveis.Venda;
+
 public class MainImovel {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    Venda apto1 = new Venda("Rua Consolação, 255", LocalDate.now(), 600000);
+    System.out.println("Endereço do apartamento: " + apto1.getEndereco());
+    System.out.println("O valor do venda é: R$ " + apto1.getValorVenda());
 
   }
 
