7b995d5a386dc89506469d9c65ec929748f364b2
GustavoSFer <gustavo.sfernander@gmail.com>
2023-07-16 20:48:27 -0300

    criando os metodos getters and setters.

--- AceleracaoJava/src/ExercicioAgenda/pessoa/Pessoa.java
+++ AceleracaoJava/src/ExercicioAgenda/pessoa/Pessoa.java
@@ -5,4 +5,28 @@ public class Pessoa {
   private int idade;
   private float altura;
 
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public int getIdade() {
+    return idade;
+  }
+
+  public void setIdade(int idade) {
+    this.idade = idade;
+  }
+
+  public float getAltura() {
+    return altura;
+  }
+
+  public void setAltura(float altura) {
+    this.altura = altura;
+  }
+
 }
