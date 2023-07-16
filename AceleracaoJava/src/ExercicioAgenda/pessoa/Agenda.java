b2881a8f8d54f055de996ebb71b6b67e322a4a72
GustavoSFer <gustavo.sfernander@gmail.com>
2023-07-16 20:54:01 -0300

    Criando a classe agenda para adicionar as pessoas e ver todas.

---
+++ AceleracaoJava/src/ExercicioAgenda/pessoa/Agenda.java
@@ -0,0 +1,17 @@
+package ExercicioAgenda.pessoa;
+
+import java.util.ArrayList;
+
+public class Agenda extends Pessoa {
+
+  private ArrayList<String> listaPessoa = new ArrayList<String>();
+
+  public void addPessoa(String nome) {
+    listaPessoa.add(nome);
+  }
+
+  public ArrayList<String> getListaPessoa() {
+    return this.listaPessoa;
+  }
+
+}
