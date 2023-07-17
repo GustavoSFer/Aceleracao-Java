08037ad0702a9d64118bee90ce7b09acc01e9678
GustavoSFer <gustavo.sfernander@gmail.com>
2023-07-16 21:00:49 -0300

    Deixando somente o nome para melhor aprendizado.

--- AceleracaoJava/src/ExercicioAgenda/pessoa/Pessoa.java
+++ AceleracaoJava/src/ExercicioAgenda/pessoa/Pessoa.java
@@ -2,8 +2,6 @@ package ExercicioAgenda.pessoa;
 
 public class Pessoa {
   private String nome;
-  private int idade;
-  private float altura;
 
   public String getNome() {
     return nome;
@@ -11,22 +9,6 @@ public class Pessoa {
 
   public void setNome(String nome) {
     this.nome = nome;
-  }
-
-  public int getIdade() {
-    return idade;
-  }
-
-  public void setIdade(int idade) {
-    this.idade = idade;
-  }
-
-  public float getAltura() {
-    return altura;
-  }
-
-  public void setAltura(float altura) {
-    this.altura = altura;
   }
 
 }
