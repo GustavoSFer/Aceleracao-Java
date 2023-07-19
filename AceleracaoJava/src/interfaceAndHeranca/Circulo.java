1ebe2ff1fdd4af777217eae8246e95344ed8ec34
GustavoSFer <gustavo.sfernander@gmail.com>
2023-07-18 22:01:11 -0300

    classe implementando a interface e faver o override do metodo (implementando o metodo)

--- AceleracaoJava/src/interfaceAndHeranca/Circulo.java
+++ AceleracaoJava/src/interfaceAndHeranca/Circulo.java
@@ -1,12 +1,18 @@
 package interfaceAndHeranca;
 
-public class Circulo {
+public class Circulo implements IArea {
 
   private double PI = Math.PI;
   private double raio;
 
   public Circulo(double raio) {
     this.raio = raio;
+  }
+
+  @Override
+  public double area() {
+    // TODO Auto-generated method stub
+    return 0;
   }
 
 }
