b05db91c1db9bf55f6ce51b33962b15c21d7f685
GustavoSFer <gustavo.sfernander@gmail.com>
2023-07-29 10:05:59 -0300

    Começando a salvar em um arquivo txt para simular um banco.

--- AceleracaoJava/src/ManipulacaoArquivos/Agenda.java
+++ AceleracaoJava/src/ManipulacaoArquivos/Agenda.java
@@ -1,23 +1,44 @@
 package ManipulacaoArquivos;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class Agenda {
   private ArrayList<Pessoa> pessoas;
+  File contatoBanco;
+
+  private static final String caminhoArquivo =
+      "C:\\Users\\Gamer\\OneDrive\\Área de Trabalho\\contatos\banco.txt";
 
   public Agenda() {
     this.pessoas = new ArrayList<Pessoa>();
+    this.contatoBanco = new File(caminhoArquivo);
   }
 
   public void addPessoa(Pessoa pessoa) {
-    this.pessoas.add(pessoa);
+    try {
+      FileWriter escreverNoArquivo = null;
+      BufferedWriter escritor = null;
+      
+      if (!this.contatoBanco.exists()) {
+        this.contatoBanco.createNewFile();
+      }
+      
+    } catch() {
+      
+    }
+   
   }
 
   public void listarAgenda() {
+    System.out.println();
     System.out.println("=== Listar Pessoas ===");
     for (Pessoa pessoa : pessoas) {
       System.out.println(pessoa.getNome() + " - " + pessoa.getPhone());
     }
     System.out.println("   -----------   ");
+    System.out.println();
   }
 }
