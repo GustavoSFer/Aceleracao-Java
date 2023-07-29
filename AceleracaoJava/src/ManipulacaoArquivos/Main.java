576ab6f32d25b4f3477b0466b330337fb463e267
GustavoSFer <gustavo.sfernander@gmail.com>
2023-07-29 09:24:42 -0300

    Adicionando pessoa na agenda.

--- AceleracaoJava/src/ManipulacaoArquivos/Main.java
+++ AceleracaoJava/src/ManipulacaoArquivos/Main.java
@@ -6,7 +6,30 @@ public class Main {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    int opcao = 0;
 
+    Agenda agenda = new Agenda();
+
+    while (opcao != 3) {
+      System.out.println("=== Escolha uma opção: ===");
+      System.out
+          .println("1- Adicionar pessoa: \n" + "2- Listar pessoas: \n" + "3- Sair do sistema: ");
+
+      switch (opcao) {
+        case 1:
+          System.out.println("Infome o nome da pessoa:");
+          String nome = scanner.nextLine();
+          System.out.println("Informe o Telefone:");
+          int phone = scanner.nextInt();
+
+          agenda.addPessoa(nome, phone);
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
+      }
+    }
 
   }
 
