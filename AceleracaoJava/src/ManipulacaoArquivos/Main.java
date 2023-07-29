dfa06ec2a47ccf231eb346fbd184c0dea1f91e20
GustavoSFer <gustavo.sfernander@gmail.com>
2023-07-29 09:17:54 -0300

    Criandi a classe main.

--- AceleracaoJava/src/ManipulacaoArquivos/Main.java
+++ AceleracaoJava/src/ManipulacaoArquivos/Main.java
@@ -1,9 +1,12 @@
 package ManipulacaoArquivos;
 
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    Scanner scanner = new Scanner(System.in);
+
 
   }
 
