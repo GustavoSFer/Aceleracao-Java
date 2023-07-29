5ec765b8984933081ece8aa88e91f5b570aaf2c0
GustavoSFer <gustavo.sfernander@gmail.com>
2023-07-29 08:48:04 -0300

    Criando a classe pessoa e seus metodos getters and setters.

---
+++ AceleracaoJava/src/ManipulacaoArquivos/Pessoa.java
@@ -0,0 +1,27 @@
+package ManipulacaoArquivos;
+
+public class Pessoa {
+  private String nome;
+  private int phone;
+
+  public Pessoa(String nome, int phone) {
+    this.nome = nome;
+    this.phone = phone;
+  }
+
+  public String getNome() {
+    return this.nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public int getPhone() {
+    return this.phone;
+  }
+
+  public void setPhone(int phone) {
+    this.phone = phone;
+  }
+}
