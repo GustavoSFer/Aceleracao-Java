3557754dfa13440ebf180b4bec961ed7c58265c7
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-01 20:57:46 -0300

    Comparando duas strings

---
+++ AceleracaoJava/src/Strings/Comparacao.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class Comparacao {
+
+  public static void main(String[] args) {
+    String nome1 = "Gustavo";
+    String nome2 = "Luiz";
+    String nome3 = "Gustavo";
+
+    System.out.println(nome1 == nome2);
+
+  }
+
+}
