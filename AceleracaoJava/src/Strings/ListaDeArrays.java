a9695aa457defc798d01584f4ad62defeb6aa375
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-01 21:05:20 -0300

    adicionando nomes em uma lista

---
+++ AceleracaoJava/src/Strings/ListaDeArrays.java
@@ -0,0 +1,20 @@
+package Strings;
+
+import java.util.ArrayList;
+
+public class ListaDeArrays {
+
+  public static void main(String[] args) {
+    ArrayList<String> nome;
+    nome = new ArrayList<String>();
+
+    nome.add("Amanda");
+    nome.add("Pedro");
+    nome.add("Lucas");
+    nome.add("Mauricio");
+    nome.add("Gusta");
+
+    System.out.println(nome);
+  }
+
+}
