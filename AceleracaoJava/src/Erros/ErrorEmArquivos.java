393eb9c409a9fe180e90fed764cb2c82787d1adb
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-10 21:48:36 -0300

    Iniciando a tratativa de escrita em arquivos.

---
+++ AceleracaoJava/src/Erros/ErrorEmArquivos.java
@@ -0,0 +1,19 @@
+package Erros;
+
+import java.io.File;
+
+public class ErrorEmArquivos {
+
+  public static void main(String[] args) {
+    String caminho = "C:\\contato";
+
+
+
+  }
+
+  public static void escreverNoArquivo(String caminhoArquivo) {
+    File meuArquivo = new File(caminhoArquivo);
+
+  }
+
+}
