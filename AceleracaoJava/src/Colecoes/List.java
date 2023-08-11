1ce9ad496f45d210a6d768803d57768591458123
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-11 20:20:06 -0300

    criando uma lista de numeros.

---
+++ AceleracaoJava/src/Colecoes/List.java
@@ -0,0 +1,20 @@
+package Colecoes;
+
+import java.util.ArrayList;
+
+public class List {
+
+  public static void main(String[] args) {
+    ArrayList<Integer> numeros = new ArrayList<Integer>();
+
+    numeros.add(1);
+    numeros.add(12);
+    numeros.add(3);
+    numeros.add(11);
+    numeros.add(7);
+    numeros.add(10);
+
+    System.out.println(numeros);
+  }
+
+}
