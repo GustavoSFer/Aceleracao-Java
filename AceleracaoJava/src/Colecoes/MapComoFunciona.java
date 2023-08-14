36b7f4567780523d2321eb6cc02664a60c4f1a4d
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-14 20:43:46 -0300

    adicionando informações em um hashmap

--- AceleracaoJava/src/Colecoes/MapComoFunciona.java
+++ AceleracaoJava/src/Colecoes/MapComoFunciona.java
@@ -7,6 +7,13 @@ public class MapComoFunciona {
   public static void main(String[] args) {
     var estudantes = new HashMap<String, String>();
 
+    estudantes.put("tarmaA", "Jão");
+    estudantes.put("TurmaB", "MairaRosa");
+
+    System.out.println(estudantes);
+
+    estudantes.put("tarmaA", "Aurelina");
+    System.out.println(estudantes);
   }
 
 }
