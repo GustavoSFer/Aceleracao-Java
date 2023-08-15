92ac3975fca0ae167690defb6eb20513cdd5ab6d
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-14 22:03:09 -0300

    testando o programa de agenda

--- AceleracaoJava/src/Colecoes/AgendaComMap.java
+++ AceleracaoJava/src/Colecoes/AgendaComMap.java
@@ -3,7 +3,13 @@ package Colecoes;
 public class AgendaComMap {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    Agenda ag = new Agenda();
+
+    ag.cadastrarPessoa("Gustavo", "11-112458744");
+    System.out.println(ag.retornaTelefone("Gustavo"));
+
+    ag.excluirContato("Gustavo");
+    System.out.println(ag.retornaTelefone("Gustavo"));
 
   }
 
