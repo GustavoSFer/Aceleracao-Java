6fd896668007e9b53bcd586b79ad68f27082ff61
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-16 21:42:05 -0300

    Criando os getters and setters

--- AceleracaoJava/src/streamAprender/Estudante.java
+++ AceleracaoJava/src/streamAprender/Estudante.java
@@ -10,4 +10,28 @@ public class Estudante {
     this.nome = nome;
     this.curso = curso;
   }
+
+  public String getMatricula() {
+    return matricula;
+  }
+
+  public void setMatricula(String matricula) {
+    this.matricula = matricula;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCurso() {
+    return curso;
+  }
+
+  public void setCurso(String curso) {
+    this.curso = curso;
+  }
 }
