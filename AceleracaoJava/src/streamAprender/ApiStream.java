76b6c13b1c8660ecf7f1b6a2f8d57185fd115431
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-16 21:48:40 -0300

    Criando uma coleção de estudante.

--- AceleracaoJava/src/streamAprender/ApiStream.java
+++ AceleracaoJava/src/streamAprender/ApiStream.java
@@ -1,10 +1,17 @@
 package streamAprender;
 
+import java.util.Collection;
+import java.util.Set;
+
 public class ApiStream {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
 
   }
+
+  public static Collection<Estudante> obterEntrada() {
+    return Set.of(new Estudante("1104", "Gustavo Fernandes", "Ciência da Computação"),
+        new Estudante("1004", "Luiz Felipe", "Administração"));
+  };
 
 }
