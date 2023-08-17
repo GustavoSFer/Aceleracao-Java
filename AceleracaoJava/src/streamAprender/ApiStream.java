3d3ab9720b4fc8553cd8e856699cac112468e65e
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-16 22:13:57 -0300

    percorrendo a coleção e lista.

--- AceleracaoJava/src/streamAprender/ApiStream.java
+++ AceleracaoJava/src/streamAprender/ApiStream.java
@@ -1,12 +1,32 @@
 package streamAprender;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ApiStream {
 
   public static void main(String[] args) {
+    Collection<Estudante> entrada = obterEntrada();
+    List<Integer> numeros = Arrays.asList(1, 3, 4, 56, 7, 8, 8, 9, 6);
 
+    numeros.stream().forEach(e -> System.out.println(e));
+    entrada.stream().forEach(e -> System.out.println(e.getNome()));
+
+    List<String> matriculas =
+        entrada.stream().filter(e -> "Ciência da Computação".equals(e.getCurso()))
+            .sorted(Comparator.comparing(Estudante::getNome)).map(Estudante::getMatricula)
+            .collect(Collectors.toList());
+    System.out.println(matriculas);
+
+    List<String> nome = entrada.stream().filter(e -> "Gustavo Fernandes".equals(e.getNome()))
+        .sorted(Comparator.comparing(Estudante::getNome)).map(Estudante::getNome)
+        .collect(Collectors.toList());
+
+    System.out.println(nome);
   }
 
   public static Collection<Estudante> obterEntrada() {
