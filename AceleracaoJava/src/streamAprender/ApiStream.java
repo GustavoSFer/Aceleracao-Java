ac47608047a6b44e4e576c340c73e6f5b5770edc
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-16 21:40:14 -0300

    Criando uma classe estudante

---
+++ AceleracaoJava/src/streamAprender/ApiStream.java
@@ -0,0 +1,10 @@
+package streamAprender;
+
+public class ApiStream {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
---
+++ AceleracaoJava/src/streamAprender/Estudante.java
@@ -0,0 +1,5 @@
+package streamAprender;
+
+public class Estudante {
+
+}
