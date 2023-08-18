4d3d107b0e259cf6fad898f8f33b819b8cebd310
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-18 14:43:56 -0300

    imprimindo cada elemento da lista

--- AceleracaoJava/src/streamAprender/BrincandoComNumeros.java
+++ AceleracaoJava/src/streamAprender/BrincandoComNumeros.java
@@ -8,6 +8,8 @@ public class BrincandoComNumeros {
   public static void main(String[] args) {
     List<Integer> numeros = Arrays.asList(1, 4, 56, 1, 56, 7, 8, 56, 4);
 
+    numeros.stream().forEach(e -> System.out.println(e));
+
   }
 
 }
