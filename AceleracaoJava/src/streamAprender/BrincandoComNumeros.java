a7c49986582d299572ac514fcec16c087b83c337
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-18 14:40:08 -0300

    Criando uma lista de numeros em um array

---
+++ AceleracaoJava/src/streamAprender/BrincandoComNumeros.java
@@ -0,0 +1,13 @@
+package streamAprender;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BrincandoComNumeros {
+
+  public static void main(String[] args) {
+    List<Integer> numeros = Arrays.asList(1, 4, 56, 1, 56, 7, 8, 56, 4);
+
+  }
+
+}
