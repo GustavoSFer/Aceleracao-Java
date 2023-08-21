d2a7b2562603837bfe719f4204141d839bc43434
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-21 20:46:44 -0300

    Aprendendo sobre time

---
+++ AceleracaoJava/src/Datas/DatasTime.java
@@ -0,0 +1,14 @@
+package Datas;
+
+import java.time.Clock;
+
+public class DatasTime {
+
+  public static void main(String[] args) {
+    Clock relogio = Clock.systemDefaultZone();
+
+    System.out.println(relogio);
+
+  }
+
+}
