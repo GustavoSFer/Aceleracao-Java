6d8f5dfc68b993ade8f57a9e07e433a66cfccd69
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-23 21:45:29 -0300

    Colocando a data que recebe no paramentro no formato brasileiro.

--- AceleracaoJava/src/Datas/AnoBissexto.java
+++ AceleracaoJava/src/Datas/AnoBissexto.java
@@ -1,18 +1,33 @@
 package Datas;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class AnoBissexto {
 
   public static void main(String[] args) {
     AnoBissexto biss = new AnoBissexto();
-    System.out.println(biss.isBissexto("2024-01-01"));
+    System.out.println(biss.isBissexto(2024));
+
+    System.out.println(biss.proximoAnoBissexto("01/01/2021"));
   }
 
-  public boolean isBissexto(String ano) {
-    LocalDate data = LocalDate.parse(ano);
+  public boolean isBissexto(int ano) {
+    String dataFormatadaISO = ano + "-01-01";
+    LocalDate data = LocalDate.parse(dataFormatadaISO);
 
     return data.isLeapYear();
+  }
+
+  public int proximoAnoBissexto(String data) {
+    String formatoData = "dd/MM/yyyy";
+    DateTimeFormatter dataFormatada = DateTimeFormatter.ofPattern(formatoData);
+    LocalDate dataformatada = LocalDate.parse(data, dataFormatada);
+
+    while (!dataformatada.isLeapYear()) {
+      dataformatada = dataformatada.plusYears(1);
+    }
+    return dataformatada.getYear();
   }
 
 }
