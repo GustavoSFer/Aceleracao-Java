c2b447e5c7c6df33cffe688be3cac188490a8ebd
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-28 21:01:40 -0300

    Configurando o Maven através do arquivo pom.xml

--- AceleracaoJava/src/MavenEGradle/AprendendoDependencia.java
+++ AceleracaoJava/src/MavenEGradle/AprendendoDependencia.java
@@ -4,8 +4,16 @@ public class AprendendoDependencia {
 
   public static void main(String[] args) {
     /*
+     * === Apache Maven ===
      * 
-    */
+     * No desenvolvimento de software, chamamos a etapa de entrega de “build”, que é quando
+     * empacotamos tudo de que precisamos para que o software funcione e disponibilizamos da forma
+     * previamente acordada para a implantação.
+     * 
+     * === Configurando o Maven através do arquivo pom.xml === Para utilizar todos os benefícios do
+     * Apache Maven, é necessário colocar um arquivo chamado “pom.xml” na raiz do seu projeto,
+     * indicando que ele é “guiado” pelo Maven.
+     */
 
   }
 
