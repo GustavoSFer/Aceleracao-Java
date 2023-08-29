ff802c314ec25ce50da3eba26a10a67900d768a4
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-28 21:04:47 -0300

    Exemplo de um pom.xml

--- AceleracaoJava/src/MavenEGradle/AprendendoDependencia.java
+++ AceleracaoJava/src/MavenEGradle/AprendendoDependencia.java
@@ -13,6 +13,31 @@ public class AprendendoDependencia {
      * === Configurando o Maven através do arquivo pom.xml === Para utilizar todos os benefícios do
      * Apache Maven, é necessário colocar um arquivo chamado “pom.xml” na raiz do seu projeto,
      * indicando que ele é “guiado” pelo Maven.
+     * 
+     * Primeiramente, antes de colocarmos nossas dependências no arquivo “pom.xml”, precisamos
+     * adicionar algumas informações básicas:
+     * 
+     * groupId: o nome dos pacotes da sua organização. artifactId: o nome do seu projeto ou produto.
+     * version: a versão em que se encontra o projeto ou produto. packaging: o tipo de arquivo que é
+     * disponibilizado no final
+     * 
+     * EXEMPLO DO ARQUIVO POM.XML
+     * 
+     * <project>
+     * 
+     * <modelVersion>4.0.0</modelVersion>
+     * 
+     * <groupId>com.trybe</groupId>
+     * 
+     * <artifactId>trybe-webapp</artifactId>
+     * 
+     * 
+     * <version>1.0-SNAPSHOT</version>
+     * 
+     * <packaging>war</packaging>
+     * 
+     * </project>
+     * 
      */
 
   }
