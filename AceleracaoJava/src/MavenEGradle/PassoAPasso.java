e5372fc024f6c87c82144070390cd01a358c121c
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-29 20:56:31 -0300

    passo a passo do gradle

--- AceleracaoJava/src/MavenEGradle/PassoAPasso.java
+++ AceleracaoJava/src/MavenEGradle/PassoAPasso.java
@@ -7,5 +7,26 @@ public class PassoAPasso {
    * 
    * 2- vamos criar umas variaveis de ambiente maven - vamos colocar o caminho da pasta no shell
    * gradle - crie a variavel de ambiente no projeto > new > file > pom.xml
+   * 
+   * fazendo no gradle um exemplo, vamos criar uma nova pasta (mkdir novaPasta) entrar dentro dela
+   * (cd novaPasta) e iniciar o nosso gradle nela (gradle init)
+   * 
+   * opções:
+   * 
+   * 1 - 2
+   * 
+   * 2 - 3
+   * 
+   * 3 - 1
+   * 
+   * 4 - 2
+   * 
+   * 5 - yes
+   * 
+   * 6 - deixe em branco
+   * 
+   * 7 - test
+   * 
+   * 8 - deixe em branco.
    */
 }
