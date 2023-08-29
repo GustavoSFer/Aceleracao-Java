f147ee4f0bf467fec8b85a4a0dd8444aebb9c335
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-29 20:46:34 -0300

    criando o pom.xml

---
+++ AceleracaoJava/src/MavenEGradle/PassoAPasso.java
@@ -0,0 +1,11 @@
+package MavenEGradle;
+
+public class PassoAPasso {
+  /**
+   * 1- baixar os arquivos em: https://maven.apache.org/download.cgi /
+   * https://gradle.org/next-steps/?version=8.3&format=bin depois tirar do ZIP
+   * 
+   * 2- vamos criar umas variaveis de ambiente maven - vamos colocar o caminho da pasta no shell
+   * gradle - crie a variavel de ambiente no projeto > new > file > pom.xml
+   */
+}
