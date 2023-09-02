1d90e727493c861f0d1cb4cc23ed999c9c091f58
GustavoSFer <gustavo.sfernander@gmail.com>
2023-09-01 21:32:05 -0300

    Criando mais dois métodos para realizar testes.

--- AceleracaoJava/src/TestJUnit/Calculadora.java
+++ AceleracaoJava/src/TestJUnit/Calculadora.java
@@ -7,4 +7,13 @@ public class Calculadora {
     resultado = n1 + n2;
     return resultado;
   }
+
+  public float subtrair(float n1, float n2) {
+    return n2 - n1;
+  }
+
+  public float multiplicar(float n1, float n2) {
+    resultado = n1 * n2;
+    return resultado;
+  }
 }
