a60cba1a3d1a6431c045ff09b55db0e18c5b7a9c
GustavoSFer <gustavo.sfernander@gmail.com>
2023-09-10 14:43:29 -0300

    Ralizando teste Instaciando a classe Banco e seu metodo construtor

--- AceleracaoJava/src/ExercicioBancoTest/BancoTest.java
+++ AceleracaoJava/src/ExercicioBancoTest/BancoTest.java
@@ -1,15 +1,21 @@
 package ExercicioBancoTest;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("Realizae teste da nossa classe calculadora!")
-class testCalculadora {
+@DisplayName("Realizae teste da nossa classe Banco!")
+class testBanco {
 
   @Test
-  @DisplayName("Realizando teste de Somar")
-  void testCalculadora() {
+  @DisplayName("Instaciando a classe Banco e seu metodo construtor")
+  void testBanco() {
+    Banco banco = new Banco("Gustavo", "16548548");
 
+    assertEquals("Gustavo", banco.getNome());
+    assertTrue(new Banco("Gustavo", "16548548") instanceof Banco);
+    assertEquals("16548548", banco.getCpf());
   }
 
 }
