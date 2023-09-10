79ec520c5b150e2f36a17569c6e9cbd63384a5b1
GustavoSFer <gustavo.sfernander@gmail.com>
2023-09-10 14:37:28 -0300

    Finalizando a nossa classe do banco. vamos começar a fazer os testes nela.

--- AceleracaoJava/src/ExercicioBancoTest/Banco.java
+++ AceleracaoJava/src/ExercicioBancoTest/Banco.java
@@ -1,14 +1,36 @@
 package ExercicioBancoTest;
 
+import java.util.ArrayList;
+
 public class Banco {
   private double valorConta;
   private String nome;
   private String cpf;
+  private ArrayList<String> transacaoTipo;
 
   public Banco(String nome, String cpf) {
     this.nome = nome;
     this.cpf = cpf;
     this.valorConta = 0;
+    this.transacaoTipo = new ArrayList<>();
+  }
+
+  public double sacar(double valor) {
+    if (this.valorConta >= valor) {
+      this.valorConta -= valor;
+      movimentacao("Sacar");
+      return this.valorConta;
+    }
+    return this.valorConta;
+  }
+
+  public double depositar(double valor) {
+    movimentacao("Depositar");
+    return this.valorConta += valor;
+  }
+
+  private void movimentacao(String tipo) {
+    this.transacaoTipo.add(tipo);
   }
 
   public double getValorConta() {
