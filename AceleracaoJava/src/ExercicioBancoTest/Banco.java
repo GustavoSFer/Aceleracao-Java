c1c52a29fcdbd8914c2eecffde26dea2895bad28
GustavoSFer <gustavo.sfernander@gmail.com>
2023-09-10 14:24:27 -0300

    Criando os metodos e o construtor.

--- AceleracaoJava/src/ExercicioBancoTest/Banco.java
+++ AceleracaoJava/src/ExercicioBancoTest/Banco.java
@@ -1,5 +1,13 @@
 package ExercicioBancoTest;
 
 public class Banco {
+  private double valorConta;
+  private String nome;
+  private String cpf;
 
+  public Banco(String nome, String cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+    this.valorConta = 0;
+  }
 }
