69bf42124bf2d737691e104e8c49e5ba67b4870d
GustavoSFer <gustavo.sfernander@gmail.com>
2023-10-02 21:01:32 -0300

    solicitando os dados para o usuario.

--- AceleracaoJava/udemy/com/udemy/banco/exerciceMain/MainConta.java
+++ AceleracaoJava/udemy/com/udemy/banco/exerciceMain/MainConta.java
@@ -1,25 +1,36 @@
 package com.udemy.banco.exerciceMain;
 
+import java.util.Scanner;
 import com.udemy.banco.exercice.Conta;
 
 public class MainConta {
 
   public static void main(String[] args) {
-    Conta comSaldo = new Conta(514, "Gustavo Frederico", 11500.00);
-    Conta semSaldo = new Conta(514, "Luiz Guilherme");
+    Scanner sc = new Scanner(System.in);
+    double valor = 0;
+
+    System.out.println("Qual o numero da conta");
+    int conta = sc.nextInt();
+    // Apenas para consumir a linha que ficou pendente do nextInt()
+    sc.nextLine();
+    System.out.println("Infome o nome do titular: ");
+    String nome = sc.nextLine();
+    System.out.println("Deseja fazer um deposito inicial? s/n");
+    String deposito = sc.nextLine();
+
+    if (deposito.equals("s")) {
+      System.out.println("Informe o valor do deposito");
+      valor = sc.nextDouble();
+    }
+
+    Conta comSaldo = new Conta(conta, nome, valor);
+    Conta semSaldo = new Conta(conta, nome);
 
     System.out.println(comSaldo);
     comSaldo.depositar(500.0);
     System.out.println(comSaldo);
-    comSaldo.sacar(11500.0);
+    comSaldo.sacar(100.0);
     System.out.println(comSaldo);
-
-    System.out.println();
-    System.out.println(semSaldo);
-    semSaldo.depositar(500.0);
-    System.out.println(semSaldo);
-    semSaldo.sacar(500.0);
-    System.out.println(semSaldo);
 
 
 
