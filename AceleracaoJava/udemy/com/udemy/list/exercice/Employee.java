4c406e31231a8f2725e01cb9ce13f1e42dcd64e4
GustavoSFer <gustavo.sfernander@gmail.com>
2023-10-05 20:07:32 -0300

    criando os construtores

--- AceleracaoJava/udemy/com/udemy/list/exercice/Employee.java
+++ AceleracaoJava/udemy/com/udemy/list/exercice/Employee.java
@@ -5,4 +5,22 @@ public class Employee {
   private String name;
   private double salario;
 
+  /**
+   * Construtor padrão
+   */
+  public Employee() {}
+
+  /**
+   * Construtor com todos os atributos
+   * 
+   * @param id
+   * @param name
+   * @param salario
+   */
+  public Employee(int id, String name, double salario) {
+    this.id = id;
+    this.name = name;
+    this.salario = salario;
+  }
+
 }
