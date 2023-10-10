1c2978ed3ad19e0dd6c84bb4990bbb56c7ffefde
GustavoSFer <gustavo.sfernander@gmail.com>
2023-10-09 21:27:40 -0300

    Iniciando o programa principal.

---
+++ AceleracaoJava/herancaPolimor/aplication/MainHeran.java
@@ -0,0 +1,18 @@
+package aplication;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MainHeran {
+
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    Scanner sc = new Scanner(System.in);
+
+
+    sc.close();
+  }
+
+}
