365d48fa5967cbefbf5f76cffdbf7aa0c00d0e6d
GustavoSFer <gustavo.sfernander@gmail.com>
2023-10-12 17:52:24 -0300

    Criando o construtor e o getters and setters

--- AceleracaoJava/excecao/com/udemy/entities/Account.java
+++ AceleracaoJava/excecao/com/udemy/entities/Account.java
@@ -6,5 +6,41 @@ public class Account {
   private double balance;
   private double widthdrawLimit;
 
+  public Account(int number, String holder, double balance, double widthdrawLimit) {
+    this.number = number;
+    this.holder = holder;
+    this.balance = balance;
+    this.widthdrawLimit = widthdrawLimit;
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  public void setNumber(int number) {
+    this.number = number;
+  }
+
+  public String getHolder() {
+    return holder;
+  }
+
+  public void setHolder(String holder) {
+    this.holder = holder;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public void setBalance(double balance) {
+    this.balance = balance;
+  }
+
+  public double getWidthdrawLimit() {
+    return widthdrawLimit;
+  }
+
+
 
 }
