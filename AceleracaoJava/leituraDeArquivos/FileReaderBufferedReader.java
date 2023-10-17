c59ea398a68f64451c47a3c874c42d65b4376e12
GustavoSFer <gustavo.sfernander@gmail.com>
2023-10-16 21:02:59 -0300

    Utilizando o FileReader e Buffered

---
+++ AceleracaoJava/leituraDeArquivos/FileReaderBufferedReader.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class FileReaderBufferedReader {
+
+  public static void main(String[] args) {
+    // Outra forma de fazer a leituro do arquivo usando o fileReader e o BufferReader
+    String path =
+        "C:\\Users\\Gamer\\git\\Aceleracao-Java\\AceleracaoJava\\leituraDeArquivos\\nomes.txt";
+    FileReader fr = null;
+    BufferedReader br = null;
+
+
+
+  }
+
+}
