c9ecd04a84f8be705cea49aa8478790096382417
GustavoSFer <gustavo.sfernander@gmail.com>
2023-10-16 21:10:02 -0300

    Fazendo o leituro com o file e buffered

--- AceleracaoJava/leituraDeArquivos/FileReaderBufferedReader.java
+++ AceleracaoJava/leituraDeArquivos/FileReaderBufferedReader.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class FileReaderBufferedReader {
 
@@ -10,7 +11,37 @@ public class FileReaderBufferedReader {
     FileReader fr = null;
     BufferedReader br = null;
 
+    try {
+      fr = new FileReader(path);
+      br = new BufferedReader(fr);
 
+      // Faz a leitura da linha
+      String line = br.readLine();
+
+      while (line != null) {
+        System.out.println(line);
+        line = br.readLine();
+      }
+    } catch (IOException e) {
+      System.out.println("Error: " + e.getMessage());
+    } finally {
+      // Neste caso precisamos fechar os dois arquivos que abrimos o fileReader e o BufferedReader e
+      // ele pode dar erro para fechar algum
+      // então neste caso precimos colocar eles dentro de um bloco try para caso der erro em algum,
+      // conseguirmos tratar ele.
+      try {
+        if (fr != null) {
+          fr.close();
+        }
+        if (br != null) {
+          br.close();
+        }
+      } catch (IOException e) {
+        System.out.println("Algo de erro para fechar um dos arquivos");
+        e.printStackTrace();
+      }
+
+    }
 
   }
 
