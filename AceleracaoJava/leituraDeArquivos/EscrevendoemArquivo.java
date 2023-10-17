876e94360f2716e55c511d921b9e279a53ba2d83
GustavoSFer <gustavo.sfernander@gmail.com>
2023-10-16 21:38:31 -0300

    explicação e com os dois tipos

--- AceleracaoJava/leituraDeArquivos/EscrevendoemArquivo.java
+++ AceleracaoJava/leituraDeArquivos/EscrevendoemArquivo.java
@@ -12,7 +12,10 @@ public class EscrevendoemArquivo {
 
     // Vamos instanciar o nosso FileWriter e o BufferedWriter, ja utilizando o nosso try with
     // resource
-    try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+    // try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+    // COLOCANDO O TRUE DEPOIS DO CAMINHO ESTAMOS INFORMANDO PARA SEMPRE ACRESCENTAR NO ARQUIVO. E
+    // NO FORMA DE CIMA (SEM O TRUE) ELE SEMPRE CRIA UM NOVO ARQUIVO
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
       for (String nome : nomes) {
         bw.write(nome);
         // Criando uma nova linha
