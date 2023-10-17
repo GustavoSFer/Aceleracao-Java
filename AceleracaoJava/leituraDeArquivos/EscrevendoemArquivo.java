ef5cc0acd15a486a85d386cb1363ef0bb082035b
GustavoSFer <gustavo.sfernander@gmail.com>
2023-10-16 21:37:19 -0300

    Escrevendo em arquivo. Deixando desta forma o arquivo é sempre criado ou feito um novo

---
+++ AceleracaoJava/leituraDeArquivos/EscrevendoemArquivo.java
@@ -0,0 +1,27 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class EscrevendoemArquivo {
+
+  public static void main(String[] args) {
+    // Vamos criar um array de nomes
+    String[] nomes = new String[] {"Pedro", "Amanda", "Luiz", "Gustavo"};
+    String path =
+        "C:\\\\Users\\\\Gamer\\\\git\\\\Aceleracao-Java\\\\AceleracaoJava\\\\leituraDeArquivos\\\\out.txt";
+
+    // Vamos instanciar o nosso FileWriter e o BufferedWriter, ja utilizando o nosso try with
+    // resource
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+      for (String nome : nomes) {
+        bw.write(nome);
+        // Criando uma nova linha
+        bw.newLine();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+}
