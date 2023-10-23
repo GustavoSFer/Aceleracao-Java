5582653d8c5ee02e5b91e1fd89031b294fdc086c
GustavoSFer <gustavo.sfernander@gmail.com>
2023-10-23 09:43:08 -0300

    Realizando as perguntas no programa principal.

---
+++ AceleracaoJava/Set/udemy/set/application/Application.java
@@ -0,0 +1,31 @@
+package udemy.set.application;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import udemy.set.entities.Aluno;
+
+public class Application {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    Set<Aluno> alunos = new HashSet<Aluno>();
+
+    System.out.print("Quantos anulos para o curso A: ");
+    int n = sc.nextInt();
+
+    for (int i = 0; i < n; i++) {
+      System.out.print("Nome do aluno: ");
+      String nome = sc.next();
+      System.out.print("Número da matricula: ");
+      int matricula = sc.nextInt();
+      Aluno aluno = new Aluno(nome, matricula, "A");
+      alunos.add(aluno);
+    }
+
+
+    System.out.print("Quantos alunos matriculados nos cursos: " + alunos.size());
+    sc.close();
+  }
+
+}
--- AceleracaoJava/Set/udemy/set/entities/Aluno.java
+++ AceleracaoJava/Set/udemy/set/entities/Aluno.java
@@ -3,10 +3,20 @@ package udemy.set.entities;
 public class Aluno {
   private String nome;
   private int numeroMatricula;
+  private String curso;
 
-  public Aluno(String nome, int numeroMatricula) {
+  public Aluno(String nome, int numeroMatricula, String curso) {
     this.nome = nome;
     this.numeroMatricula = numeroMatricula;
+    this.curso = curso;
+  }
+
+  public String getCurso() {
+    return curso;
+  }
+
+  public void setCurso(String curso) {
+    this.curso = curso;
   }
 
   public String getNome() {
