a54aa4de2fd00d6fa56d5270d1c43eb9b958d3f9
GustavoSFer <gustavo.sfernander@gmail.com>
2023-10-23 09:51:59 -0300

    Iniciando o prendizado do MAP

---
+++ AceleracaoJava/Map/udemy/map/entities/Candidato.java
@@ -0,0 +1,29 @@
+package udemy.map.entities;
+
+public class Candidato {
+  private String nome;
+  private int votos;
+
+  public Candidato(String nome, int votos) {
+    this.nome = nome;
+    this.votos = votos;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+
+}
