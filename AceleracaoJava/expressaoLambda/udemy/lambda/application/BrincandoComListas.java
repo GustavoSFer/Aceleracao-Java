8854dea09b801b3e3266ca69b723b4cb88a7227f
GustavoSFer <gustavo.sfernander@gmail.com>
2023-10-23 14:19:43 -0300

    Adicionando produtos a nossa lista.

--- AceleracaoJava/expressaoLambda/udemy/lambda/application/BrincandoComListas.java
+++ AceleracaoJava/expressaoLambda/udemy/lambda/application/BrincandoComListas.java
@@ -1,11 +1,20 @@
 package udemy.lambda.application;
 
+import java.util.ArrayList;
 import java.util.List;
+import udemy.lambda.entities.Produto;
 
 public class BrincandoComListas {
 
   public static void main(String[] args) {
-    List<String> produtos;
+    List<Produto> produtos = new ArrayList<>();
+
+    produtos.add(new Produto("Celular", 1500.00));
+    produtos.add(new Produto("PC", 2500.00));
+    produtos.add(new Produto("Geladeira", 3000.00));
+    produtos.add(new Produto("Carregador", 500.00));
+    produtos.add(new Produto("caixa de som", 1200.00));
+    produtos.add(new Produto("Mouse", 60.00));
 
   }
 
