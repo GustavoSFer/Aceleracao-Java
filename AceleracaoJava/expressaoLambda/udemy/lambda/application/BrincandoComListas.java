fe5d0f7fd3a8b3ca5abf6aa93eabfcf62cbfbeb4
GustavoSFer <gustavo.sfernander@gmail.com>
2023-10-23 14:13:49 -0300

    Vamos aprender sobre expressão lambda

---
+++ AceleracaoJava/expressaoLambda/udemy/lambda/application/BrincandoComListas.java
@@ -0,0 +1,12 @@
+package udemy.lambda.application;
+
+import java.util.List;
+
+public class BrincandoComListas {
+
+  public static void main(String[] args) {
+    List<String> produtos;
+
+  }
+
+}
