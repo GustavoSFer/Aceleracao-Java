1008e5a729bb18940342fc7dcb0ed01980818055
GustavoSFer <gustavo.sfernander@gmail.com>
2023-10-23 14:35:19 -0300

    javadoc

--- AceleracaoJava/expressaoLambda/udemy/lambda/application/MeuComparador.java
+++ AceleracaoJava/expressaoLambda/udemy/lambda/application/MeuComparador.java
@@ -3,6 +3,10 @@ package udemy.lambda.application;
 import java.util.Comparator;
 import udemy.lambda.entities.Produto;
 
+/**
+ * Criando uma classe de comparação que implementa de Comparator, para passarmos o objeto um e
+ * comparar com o objeto 2 assim retornando valor 0 se for igual (int)
+ */
 public class MeuComparador implements Comparator<Produto> {
 
   @Override
